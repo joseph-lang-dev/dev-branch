b1932a836e4ef87dd482e50380b7fd91114e8c2c
Joseph Lang <langj6757@gmail.com>
2025-05-22 13:03:01 -0700

    feat: Configure GitHub OAuth authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
-const Home = () => {
-  return <div>Home</div>;
+import { signIn } from "@/auth";
+
+const Home = async () => {
+  return (
+    <div>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("github");
+        }}
+      >
+        <button type="submit">Signin with GitHub</button>
+      </form>
+    </div>
+  );
 };
 export default Home;
